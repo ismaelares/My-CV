5c8ac8c27847d75c9deb3a3fded89dcf9dcb8b2e
ismaelares <ismael.lardom@gmail.com>
2020-10-01 12:16:15 -0700

    Updates

---
+++ precache-manifest.523ab0f537bd9bc1203f1b87f1b691e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f9d30ef2b13d4c21552795d0eec1f5",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "d1b90c991b1c5f7d6f11",
+    "url": "/my-cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "06b31ef00d4b43bb8a89",
+    "url": "/my-cv/static/js/2.bf5f7c64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-cv/static/js/2.bf5f7c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b90c991b1c5f7d6f11",
+    "url": "/my-cv/static/js/main.dcdeecb1.chunk.js"
+  },
+  {
+    "revision": "59b714c33bdd7cb64d9c",
+    "url": "/my-cv/static/js/runtime-main.c75d1202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
